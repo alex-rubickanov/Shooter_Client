c6a3311b89a768015f98222d643466df296d973c
rubickanov <alexander850m@gmail.com>
2024-03-08 12:48:46 +0400

    Added position to PawnSpawnPacket

--- ShooterNetworkingPackets/ShooterNetworkingPackets/PawnSpawnPacket.cs
+++ ShooterNetworkingPackets/ShooterNetworkingPackets/PawnSpawnPacket.cs
@@ -2,17 +2,31 @@ namespace ShooterNetwork
 {
     public class PawnSpawnPacket : BasePacket
     {
-        
+        private Vector2 position;
+        public Vector2 Position => position; 
         public PawnSpawnPacket()
         {
         }
         
-        public PawnSpawnPacket(PlayerData playerData) : base(PacketType.PlayerPawnSpawn, playerData)
+        public PawnSpawnPacket(Vector2 position, PlayerData playerData) : base(PacketType.PlayerPawnSpawn, playerData)
         {
+            this.position = position;
+        }
+        
+        public override byte[] Serialize()
+        {
+            BeginSerialize();
+            sbw.Write(Position.X);
+            sbw.Write(Position.Y);
+            return EndSerialize();
         }
         
         public new PawnSpawnPacket Deserialize(byte[] buffer)
         {
+            BeginDeserialize(buffer);
+            position.X = dbr.ReadSingle();
+            position.Y = dbr.ReadSingle();
+            EndDeserialize();
             return (PawnSpawnPacket) base.Deserialize(buffer);
         }
     }
